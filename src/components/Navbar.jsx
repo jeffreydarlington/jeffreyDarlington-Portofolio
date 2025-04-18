cc906cd46c14429913a4f1a02d63d82a108f89ff
jeffreydarlington <jeffrey2md@gmail.com>
2025-04-17 21:51:24 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) =>{
         document.body.style.overflow = menuOpen ? "hidden" : "";
     },[menuOpen])
 
-    return( <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg boarder-b border-white/10 shadow-lg">
+    return( <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blue-lg boarder-b border-white/10 shadow-lg">
         <div className="max-w-5xl mx-auto px-4">
             <div className="flex justify-between items-center h-16 ">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
