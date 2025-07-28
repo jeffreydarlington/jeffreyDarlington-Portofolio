6033ea4efcd8cb14d264c2484157c04c39cdc709
jeffreydarlington <jeffrey2md@gmail.com>
2025-07-28 19:08:45 -0400

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -163,7 +163,7 @@ export const Projects = () =>{
                         {["C++"].map((tech, key)=>(
                             <span 
                             key={key}
-                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                className="bg-pink-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
                                                 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
                             "
                             >
