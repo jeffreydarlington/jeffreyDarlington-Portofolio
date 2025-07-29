7bdebe137696e656791585fcac0e6b90c244b8a4
jeffreydarlington <jeffrey2md@gmail.com>
2025-07-28 21:21:16 -0400

    Test

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () =>{
             <RevealOnScroll>
             <div className="text-center z-10 px-4">
                 <h1 className="text-5xl md:text-7xl font-bold mb6 bg-gradient-to-r from-pink-500 to-pink-400 bg-clip-text text-transparent leading-right"
-                >Hi, I'm Jeffrey Darlington
+                >H, I'm Jeffrey Darlington
                 </h1>
 
                 <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
