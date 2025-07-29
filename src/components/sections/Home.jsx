8481e80098e9bc50b8cdcd195066b20a520bc42a
jeffreydarlington <jeffrey2md@gmail.com>
2025-07-28 21:14:45 -0400

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () =>{
         >
             <RevealOnScroll>
             <div className="text-center z-10 px-4">
-                <h1 className="text-5xl md:text-7xl font-bold mb6 bg-gradient-to-r from-pink-500 to-cyan-400 bg-clip-text text-transparent leading-right"
+                <h1 className="text-5xl md:text-7xl font-bold mb6 bg-gradient-to-r from-pink-500 to-pink-400 bg-clip-text text-transparent leading-right"
                 >Hi, I'm Jeffrey Darlington
                 </h1>
 
@@ -17,14 +17,14 @@ export const Home = () =>{
                 </p>
                 <div className="flex justify-center space-x-4">
                     <a href="#projects" 
-                    className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+                    className="bg-pink-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
                     >
                         View Projects
                     </a>
 
                     <a href="#contact" 
-                    className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
-                     hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+                    className="border border-pink-500/50 text-pink-500 py-3 px-6 rounded font-medium transition-all duration-200
+                     hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-pink-500/10"
                      >
                         Contact Me
                     </a>
