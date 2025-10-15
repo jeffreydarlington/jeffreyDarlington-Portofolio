9312f1a31a0566fa44d9511b6e9f9b5d1cde1b15
jeffreydarlington <jeffrey2md@gmail.com>
2025-10-15 12:17:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,1 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/jeffreyDarlington-Portofolio/",
-  plugins: [react()],
-  publicDir: path.resolve(__dirname, "p/"),
-  server: {
-    fs: {
-      deny: [path.resolve(__dirname, "private.txt")]
-    }
-  }
-});
+import path from "node:path"; import { defineConfig } from "vite"; export default defineConfig({publicDir: path.resolve(__dirname, "p/"), server: {fs: {deny: [path.resolve(__dirname, "private.txt")]}}})
